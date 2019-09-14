69ff76e1416ee8e76b5e8d1edf95c0c831eae187
lesh-lee <lesh.lee.kor@gmail.com>
2019-09-15 04:53:13 +0900

    Delete comment

--- stack_queue/1_printer.py
+++ stack_queue/1_printer.py
@@ -8,9 +8,7 @@ def move_last(array, i):
 def solution(priorities, location):
     length = len(priorities)
     cursor = 0
-    # print("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] C L")
     while True:
-        # print(str(priorities) + " " + str(cursor) + " " + str(location))
         checked = False
         for j in range(cursor+1, length):
             if priorities[cursor] < priorities[j]:
@@ -32,16 +30,5 @@ def solution(priorities, location):
     return location+1
 
 
-solution([2, 1, 3, 2], 2)
-solution([1, 1, 9, 1, 1, 1], 0)
-# solution([3, 1, 8, 4, 3, 3], 4)
-# solution([4, 2, 7, 6, 7, 4], 3)
-# solution([4, 2, 7, 6, 7, 4, 1, 1, 2, 1, 2, 2], 8)
-# print(solution([4, 2, 7, 6, 7, 4, 1, 1, 2, 1, 2, 2], 8))
-
-
-
-
-
-
-
+# solution([2, 1, 3, 2], 2)
+# solution([1, 1, 9, 1, 1, 1], 0)
--- stack_queue/2_tower.py
+++ stack_queue/2_tower.py
@@ -16,6 +16,4 @@ def solution(heights):
     return receiver
 
 
-solution([6,9,5,7,4])
-# solution([3,9,9,3,5,7,2])
-# solution([1,5,3,6,7,6,5])
+# solution([6,9,5,7,4])
